cc172c56fb79377881abf7490efd87d41230027d
karthiktavva123 <karthiktavva64@gmail.com>
2026-04-28 17:33:51 +0000

    updated app

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-
-    return "DevOps CI/CD Working"
-
+    return "DevOps CI/CD Working 🚀"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.run(host='0.0.0.0', port=5000)
